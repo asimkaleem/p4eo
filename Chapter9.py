b924c6a1b0545822f9db555034d0def0f11d1c2e
asim <asim.kaleem@gmail.com>
2014-12-09 11:22:06 +0500

    Added Chapter 8

--- Chapter9.py
+++ Chapter9.py
@@ -1,2 +1,37 @@
 __author__ = 'asim'
-asdf
+# myList = list()
+# print myList
+#
+# myCollection = dict()
+#
+# print myCollection
+# myCollection[1] = 'World'
+# print myCollection
+# print len(myCollection)
+#
+# myCollection1 = {1:"Hello World", 2 : "HOw are you"}
+# print "My Second Colleciton", myCollection1
+# for item in myCollection1:
+#     print myCollection1[item]
+
+# eng2sp = dict()
+# eng2sp = {'one': 'uno', 'two': 'dos', 'three': 'tres'}
+# print "Lenght of dictionary: ", len(eng2sp)
+# print "Content of dictionary: ", eng2sp
+# print "Does translation of One exist? ", 'one' in eng2sp
+# vals = eng2sp.values()
+# print vals
+
+#-----------------------------------------------------------------------------------------------------------------------
+# Exercise 9.1 Write a program that reads the words in words.txt and stores them
+# as keys in a dictionary. It doesn’t matter what the values are. Then you can use
+# the in operator as a fast way to check whether a string is in the dictionary.
+#-------------------------# Exercise 9.1 Write a program that reads the words in words.txt and stores them
+# as keys in a dictionary. It doesn’t matter what the values are. Then you can use
+# the in operator as a fast way to check whether a string is in the dictionary.
+----------------------------------------------------------------------------------------------
+
+
+
+
+
