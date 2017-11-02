afdb1479888acc73c3a019f0eaa6f13378892483
asim kaleem khan <asim@ebryx.com>
2017-11-02 18:13:06 +0500

    Revised Chapter 7

--- Chapter6.py
+++ Chapter6.py
@@ -101,8 +101,8 @@
 # Define a function that computes the length of a given list or string. (It is true that Python has the len()
 # function built in, but writing it yourself is nevertheless a good exercise.)
 my_string = input("Please enter a string for length count: ")
-str_len =
-for letter
+
+
 
 
 
